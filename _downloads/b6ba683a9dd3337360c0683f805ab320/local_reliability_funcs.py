6692c478b0f94e4c856acf6becb4328b387fc1d9
dimtsap <dimtsap@users.noreply.github.com>
2025-05-13 10:25:33 +0000

    deploy: 9f13c1fdefa01b688661cf3594938e6fccbc47f6

---
+++ _downloads/b6ba683a9dd3337360c0683f805ab320/local_reliability_funcs.py
@@ -0,0 +1,11 @@
+"""
+
+Correlated gaussian model for tempering examples
+====================================================================
+
+"""
+import numpy as np
+
+
+def correlated_gaussian(samples, b_eff, d):
+    return [b_eff * np.sqrt(d) - np.sum(samples[i, :]) for i in range(samples.shape[0])]
